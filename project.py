9a8154a0094dfe30195f01b5e5dc9a8e121b050d
snehadesh <deshmukhsneha926@gmail.com>
2021-06-30 17:27:01 +0530

    first commit

---
+++ project.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+print("Hello user! find the news related to your interest here.")
+selected_tag = input("select tag - Education/Business/politics ")
+print(selected_tag)
+URL = 'https://timesofindia.indiatimes.com/' + selected_tag
+
+page = requests.get(URL)
+soup = BeautifulSoup(page.content,'html.parser')
+results = soup.find_all('span',class_="w_tle")
+listof = []
+for title in results:
+    #print(title, end='\n'*2)
+    news= title.find('a')
+    listof.append(news)
+#print(listof)
+n = 1       
+for i in listof[:10]:
+    print((str(n) + '.'), i.text)
+    n += 1
+    if None in (listof):
+        continue
+
+
+
+
+
+
