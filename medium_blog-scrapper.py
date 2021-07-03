6d2c3794b536a33aaa40329769c51ed232ec29ef
snehadesh <deshmukhsneha926@gmail.com>
2021-07-03 22:38:13 +0530

    added anather scapper

---
+++ medium_blog-scrapper.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+print("Hello,find your desired job here!")
+keyword= input("Enter the BLOG title you are searching for-  ")
+print(keyword)
+
+URL = 'https://medium.com/search?q=' + keyword
+page = requests.get(URL)
+soup = BeautifulSoup(page.content,'html.parser')
+results = soup.find_all('div',class_="postArticle-content")
+link_of = soup.find_all("a", href=True)
+# print(results)
+list_of = []
+for blog in results:
+    #print(blog, end='\n'*2)
+    blog_list= blog.find('h3')
+    list_of.append(blog_list)
+n = 1    
+for i in list_of:
+    if i == None:
+        continue
+    for el in link_of:
+        link =el['href']
+    print((str(n) + '.'),i.text)
+    n += 1
+    print("Link --",link,end='\n'*2)
+   
+        
+
+    
